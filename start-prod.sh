c90413e64e3478af67f55278a8b08a2e413521b7
Z User <z@container>
2026-04-10 01:49:42 +0000

    703f6aa4-b7fe-468d-8861-79631b4fca16

--- start-prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  echo "[$(date)] Starting production server..." >> /tmp/next-prod.log
-  npx next start -p 3000 >> /tmp/next-prod.log 2>&1
-  EXIT=$?
-  echo "[$(date)] Server exited with code $EXIT, restarting in 2s..." >> /tmp/next-prod.log
-  sleep 2
-done
